de9b36111a8df771339e425395fd38bfdf9e4581
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-08-08 00:10:20 -0500

    Pruebas de Strings

---
+++ src/utils/strings.js
@@ -0,0 +1,9 @@
+export const saluda = (name) => {
+  return "Hola soy" + name + " 6";
+};
+export const despide = () => {
+  return "Bye";
+};
+export const getId = () => {
+  return "44-123";
+};
---
+++ src/utils/strings.test.js
@@ -0,0 +1,17 @@
+import { saluda, getId, despide } from "./strings";
+describe("Pruebas de strings", () => {
+  const saludo = saluda("Eisten");
+  const id = getId("Eisten");
+  const saludoDespide = despide();
+  test("Probando la funcion saluda", () => {
+    expect(saludo).toMatch("Hola soy");
+    // expect(saludo).toMatch(/\d/);
+  });
+  test("Probando la funcion getId", () => {
+    expect(id).toMatch(/\d{2}-\d{3}/);
+  });
+  test("Probando la funcion despide", () => {
+    expect(saludoDespide).toMatch("Bye");
+    // expect(saludoDespide).not.toMatch("Bye");
+  });
+});
